ae62754a9e5e091d754d22275e5461ed04a24431
Mateus-Kent <benriba2003@gmail.com>
2021-12-04 18:31:37 -0300

    style: remove width and height

--- src/components/BlackButton/styles.ts
+++ src/components/BlackButton/styles.ts
@@ -6,18 +6,16 @@ export const styles = StyleSheet.create({
   container: {
     justifyContent: "center",
     alignItems: "center",
-    height: 45,
     backgroundColor: '#0a0a0a',
     borderRadius: 26,
-    width: 270,
     borderColor: theme.COLORS.white,
     borderWidth: 0.5,
     borderStyle: 'solid',
   },
   textButton: {
-   fontFamily: theme.FONTS.title700,
-   fontSize: 13,
-   color: theme.COLORS.white,
-   marginBottom: 2
+    fontFamily: theme.FONTS.title700,
+    fontSize: 13,
+    color: theme.COLORS.white,
+    marginBottom: 2
   }
 });
