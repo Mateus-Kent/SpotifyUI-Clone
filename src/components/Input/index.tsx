9c0af06613ce589ef1e686029d34c46439e3e316
Mateus-Kent <benriba2003@gmail.com>
2021-12-04 00:44:42 -0300

    feat: create the input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,64 @@
+import React, { useRef, useEffect, useCallback } from "react";
+import { TextInput, TextInputProps, Text } from "react-native";
+import { useField } from "@unform/core";
+import { style } from "./styles";
+
+interface InputProps extends TextInputProps {
+  name: string;
+  label?: string;
+  placeholder?: string;
+  marginBottom?: number;
+}
+
+interface InputReference extends TextInput {
+  value: string;
+}
+
+export function Input({ name, label, onChangeText, ...rest }: InputProps) {
+  const inputRef = useRef<InputReference>(null);
+  const { fieldName, registerField, defaultValue = "", error } = useField(name);
+  useEffect(() => {
+    if (inputRef.current) inputRef.current.value = defaultValue;
+  }, [defaultValue]);
+  useEffect(() => {
+    registerField<string>({
+      name: fieldName,
+      ref: inputRef.current,
+      getValue() {
+        if (inputRef.current) return inputRef.current.value;
+        return "";
+      },
+      setValue(ref, value) {
+        if (inputRef.current) {
+          inputRef.current.setNativeProps({ text: value });
+          inputRef.current.value = value;
+        }
+      },
+      clearValue() {
+        if (inputRef.current) {
+          inputRef.current.setNativeProps({ text: "" });
+          inputRef.current.value = "";
+        }
+      },
+    });
+  }, [fieldName, registerField]);
+  const handleChangeText = useCallback(
+    (value: string) => {
+      if (inputRef.current) inputRef.current.value = value;
+      if (onChangeText) onChangeText(value);
+    },
+    [onChangeText]
+  );
+  return (
+    <>
+      {label && <Text>{label}</Text>}
+      <TextInput
+        style={style.styleInput}
+        ref={inputRef}
+        onChangeText={handleChangeText}
+        defaultValue={defaultValue}
+        {...rest}
+      />
+    </>
+  );
+}
