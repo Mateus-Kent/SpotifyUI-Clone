c4d1939d8b0f2dfd44550ee8c0540f5496b9164d
Mateus-Kent <benriba2003@gmail.com>
2021-12-04 12:25:37 -0300

    feat: add ReactButtonProps to the component

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Text } from "react-native";
-import { RectButton } from "react-native-gesture-handler";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
 
 import { styles } from "./styles";
 
 type ButtonProps = {
   children: String;
   marginBottom?: number;
-};
+} & RectButtonProps;
 
 export function LoginButton({ children, ...rest }: ButtonProps) {
   return (
