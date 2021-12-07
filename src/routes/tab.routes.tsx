9ee465b7369ab4522057d92eeb7f1aa7bd42de28
Mateus-Kent <benriba2003@gmail.com>
2021-12-07 00:34:11 -0300

    style: stylize the tabBottomNavigation

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -1,14 +1,38 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Entypo } from "@expo/vector-icons";
 
 import { Home } from "../screens/Home";
+import { theme } from "../global/styles";
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function TabRoutes() {
   return (
-    <Navigator>
-      <Screen name="Main" component={Home} />
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          height: 48,
+          backgroundColor: theme.COLORS.grayPrimary,
+        },
+      }}
+    >
+      <Screen
+        name="Início"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused, size }) => {
+            return (
+              <Entypo
+                name="home"
+                size={size}
+                color={focused ? theme.COLORS.white : "#A7A7A7"}
+              />
+            );
+          },
+        }}
+      />
     </Navigator>
   );
 }
