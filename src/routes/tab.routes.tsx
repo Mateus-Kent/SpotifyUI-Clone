d976a24ea1379eae56c2ffacf115a07b66db10c6
Mateus-Kent <benriba2003@gmail.com>
2021-12-08 19:43:52 -0300

    style: stylize the BottomTabNavigation

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Entypo } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 
 import { Home } from "../screens/Home";
+import { Search } from "../screens/Search";
+import { Library } from "../screens/Library";
+
 import { theme } from "../global/styles";
 
 const { Navigator, Screen } = createBottomTabNavigator();
@@ -12,9 +17,24 @@ export function TabRoutes() {
     <Navigator
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: theme.COLORS.white,
+        tabBarInactiveTintColor: "#A7A7A7",
         tabBarStyle: {
           height: 48,
           backgroundColor: theme.COLORS.grayPrimary,
+          borderTopWidth: 0,
+          alignItems: "center",
+          justifyContent: "center",
+        },
+        tabBarIconStyle: {
+          flex: 0,
+          width: 24,
+          height: 24,
+        },
+        tabBarLabelStyle: {
+          fontFamily: theme.FONTS.title500,
+          fontSize: 9,
+          marginTop: 3,
         },
       }}
     >
@@ -26,6 +46,38 @@ export function TabRoutes() {
             return (
               <Entypo
                 name="home"
+                size={size}
+                color={focused ? theme.COLORS.white : "#A7A7A7"}
+              />
+            );
+          },
+        }}
+      />
+
+      <Screen
+        name="Buscar"
+        component={Search}
+        options={{
+          tabBarIcon: ({ focused, size }) => {
+            return (
+              <Ionicons
+                name="md-search"
+                size={size}
+                color={focused ? theme.COLORS.white : "#A7A7A7"}
+              />
+            );
+          },
+        }}
+      />
+
+      <Screen
+        name="Biblioteca"
+        component={Library}
+        options={{
+          tabBarIcon: ({ focused, size }) => {
+            return (
+              <MaterialIcons
+                name="library-music"
                 size={size}
                 color={focused ? theme.COLORS.white : "#A7A7A7"}
               />
---
+++ src/screens/Library/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+
+import { styles } from "./styles";
+
+export function Library() {
+  return (
+    <View style={styles.container}>
+      <Text>Library</Text>
+    </View>
+  );
+}
