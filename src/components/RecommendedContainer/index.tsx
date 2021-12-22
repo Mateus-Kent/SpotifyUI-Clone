44c15ebcb13dc6be816cf394c4aae5d248876c00
Mateus-Kent <benriba2003@gmail.com>
2021-12-22 20:47:27 -0300

    feat(components): create RecommendedContainer

---
+++ src/components/RecommendedContainer/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+
+import { styles } from "./styles";
+
+type SongJson = {
+  song: any;
+};
+
+export function RecommendedContainer({ song }: SongJson) {
+  return (
+    <View style={styles.container}>
+      {song.slice(0, 6).map(() => {
+        <View style={styles.songCard} key={song.id}>
+          <Image style={styles.containerImage} source={song.photo} />
+          <Text style={styles.containerTitle}>{song.name}</Text>
+        </View>;
+      })}
+    </View>
+  );
+}
---
+++ src/components/RecommendedContainer/styles.ts
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: 185,
+    width: 380,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  songCard: {
+    height: 56,
+    width: 186,
+    backgroundColor: theme.COLORS.graySecondary,
+    paddingHorizontal: 8,
+    paddingBottom: 8,
+    flexDirection: "row",
+  },
+  containerImage: {
+    width: 56,
+    height: 56,
+  },
+  containerTitle: {
+    fontFamily: theme.FONTS.title500,
+    fontSize: 14,
+    color: theme.COLORS.white,
+  },
+});
